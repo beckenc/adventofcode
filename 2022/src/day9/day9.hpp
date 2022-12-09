c6e1f0594c1e78d10a264082d4dfff69bc30e16e
beckenc <github@claus-beckenbauer.de>
2022-12-09 09:32:37 +0100

    Day9, Part1. Input and Output of testdata

--- 2022/src/day9/day9.hpp
+++ 2022/src/day9/day9.hpp
@@ -1,6 +1,66 @@
 #pragma once
+#include <algorithm>
+#include <istream>
+#include <ranges>
+#include <tuple>
+#include <vector>
 
 namespace aoc::day9 {
+
+enum class Direction : char { up = 'U', down = 'D', left = 'L', right = 'R' };
+
+class Motion {
+ public:
+  typedef unsigned step_count_t;
+  typedef std::tuple<Direction, step_count_t> motion_t;
+
+  auto& get() { return _motion; }
+  auto direction() const { return std::get<0>(_motion); }
+  auto num_steps() const { return std::get<1>(_motion); }
+
+ private:
+  motion_t _motion;
+};
+
+class Motions {
+ public:
+  typedef std::vector<Motion> motions_t;
+  auto append(Motion&& motion) { _motions.emplace_back(std::move(motion)); }
+
+  auto get() { return _motions; }
+  auto get() const { return _motions; }  
+
+ private:
+  motions_t _motions;
+};
+
+inline std::istream& operator>>(std::istream& is, Direction& direction) {
+  auto d = char{};
+  is >> d;
+  direction = static_cast<Direction>(d);
+  return is;
+}
+
+inline std::istream& operator>>(std::istream& is, Motion& motion) {
+  auto& [dir, count] = motion.get();
+  is >> dir >> count;
+  return is;
+}
+
+inline std::ostream& operator<<(std::ostream& os, const Direction& direction) {
+  os << static_cast<char>(direction);
+  return os;
+}
+
+inline std::ostream& operator<<(std::ostream& os, const Motion& motion) {
+  os << motion.direction() << " " << motion.num_steps();
+  return os;
+}
+
+inline std::ostream& operator<<(std::ostream& os, const Motions& motions) {
+  std::ranges::for_each(motions.get(), [&os](auto&& motion) { os << motion << std::endl; });
+  return os;
+}
 
 auto main_pt1(int argc, char** argv) -> int;
 auto main_pt2(int argc, char** argv) -> int;
--- 2022/test/day9/day9.test.cpp
+++ 2022/test/day9/day9.test.cpp
@@ -1,13 +1,27 @@
 #include "day9/day9.hpp"
 
+#include <iostream>
+#include <sstream>
+#include <vector>
+
 #include "gtest/gtest.h"
 
 namespace aoc::day9 {
 
 TEST(SimpleTestPt1, day9) {
+  auto input = std::vector<std::string>{"R 4", "U 4", "L 3", "D 1",
+                                        "R 4", "D 1", "L 5", "R 2"};
+
+  auto motions = Motions{};
+  for (auto in : input) {
+    auto motion = Motion{};
+    std::stringstream{in} >> motion;
+    motions.append(std::move(motion));
+  }
+
+  std::cout << motions << std::endl;
 }
 
-TEST(SimpleTestPt2, day9) {
-}
+TEST(SimpleTestPt2, day9) {}
 
 }  // namespace aoc::day9
