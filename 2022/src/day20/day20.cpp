e87f5987626ba2f99104b66ebd5559523dd94c72
beckenc <github@claus-beckenbauer.de>
2022-12-23 16:04:39 +0100

    Day20, Part2

--- 2022/src/day20/day20.cpp
+++ 2022/src/day20/day20.cpp
@@ -5,48 +5,58 @@
 #include <cmath>
 #include <iostream>
 #include <ranges>
+#include <set>
 
 namespace aoc::day20 {
 
-auto List::nextpos(auto current_pos, auto increment) const {
+auto RefList::currentpos(auto number) const {
+  auto numpos = std::ranges::find_if(*this, [&number](auto &&it) {  //
+    return it->id == number.id;
+  });
+  assert(numpos != std::end(*this));
+  return std::distance(std::begin(*this), numpos);
+};
+
+auto RefList::nextpos(auto current, auto increment) const {
   auto max = std::ssize(*this) - 1;
   if (std::signbit(increment)) {
     increment += ((std::abs(increment) / max) + 1) * max;
   }
-  auto pos = (current_pos + increment) % max;
+  auto next = (current + increment) % max;
   auto it = std::begin(*this);
-  return it + pos;
+  return std::make_pair(it + next, next);
 };
+
+auto RefList::shuffle() {
+  for (auto li = _list.begin(); li != _list.end(); ++li) {
+    auto current = currentpos(*li);
+    auto [next, pos] = nextpos(current, li->value);
+    erase(std::begin(*this) + current);
+    insert(next, li);
+  }
+}
 
 auto main_pt1(int argc, char **argv) -> int {  //
   auto list = List{};
   std::ranges::for_each(
-      std::ranges::istream_view<Number>(std::cin), [&list](auto &&number) {
+      std::ranges::istream_view<List::value_type>(std::cin),
+      [&list, pos = 0](auto &&number) mutable {
         list.emplace_back(std::forward<decltype(number)>(number));
       });
 
-  auto begin = [&list](auto li) {
-    for (; li != std::end(list) && li->touched; std::advance(li, 1))
-      ;
-    return li;
-  };
+  auto shuffled = RefList{list};
+  shuffled.shuffle();
 
-  for (auto li = begin(std::begin(list)); li != std::end(list);
-       li = begin(li)) {
-    auto value = li->value;
-    auto nxtpos = list.nextpos(std::distance(std::begin(list), li), value);
-    list.erase(li);
-    list.insert(nxtpos, {value, true});
-  }
-
-  auto pos0 = std::distance(std::begin(list), std::ranges::find(list, Number{}));
+  auto pos0 = std::distance(
+      std::begin(shuffled),
+      std::ranges::find_if(shuffled, [](auto &&n) { return n->value == 0; }));
 
   auto sum = int64_t{0};
   for (auto pos : std::vector<int>{1000, 2000, 3000}) {
-    auto npos = (pos + pos0) % std::ssize(list);
-    auto it = std::begin(list);
+    auto npos = (pos + pos0) % std::ssize(shuffled);
+    auto it = std::begin(shuffled);
     std::advance(it, npos);
-    sum += it->value;
+    sum += (*it)->value;
   }
 
   std::cout << "Part1:" << sum << std::endl;
@@ -55,6 +65,33 @@ auto main_pt1(int argc, char **argv) -> int {  //
 }
 
 auto main_pt2(int argc, char **argv) -> int {  //
+  auto list = List{};
+  std::ranges::for_each(
+      std::ranges::istream_view<List::value_type>(std::cin),
+      [&list, pos = 0](auto &&number) mutable {
+        number.value *= 811589153;
+        list.emplace_back(std::forward<decltype(number)>(number));
+      });
+
+  auto shuffled = RefList{list};
+  for (auto i : std::views::iota(0) | std::views::take(10)) {
+    shuffled.shuffle();
+  }
+
+  auto pos0 = std::distance(
+      std::begin(shuffled),
+      std::ranges::find_if(shuffled, [](auto &&n) { return n->value == 0; }));
+
+  auto sum = int64_t{0};
+  for (auto pos : std::vector<int>{1000, 2000, 3000}) {
+    auto npos = (pos + pos0) % std::ssize(shuffled);
+    auto it = std::begin(shuffled);
+    std::advance(it, npos);
+    sum += (*it)->value;
+  }
+
+  std::cout << "Part2:" << sum << std::endl;
+
   return 0;
 }
 }  // namespace aoc::day20
--- 2022/src/day20/day20.hpp
+++ 2022/src/day20/day20.hpp
@@ -7,17 +7,32 @@ namespace aoc::day20 {
 
 struct Number {
   int64_t value = 0;
-  bool touched = false;
-  auto operator==(const Number& rhs) const { return value == rhs.value; };
-  auto operator<=>(const Number& rhs) const { return value <=> rhs.value; };
+  int id = 0;
+  auto operator==(const Number& rhs) const { return id == rhs.id; };
+  auto operator<=>(const Number& rhs) const { return id <=> rhs.id; };
 };
 
-struct List : public std::vector<Number> {
+struct List : public std::vector<Number> {  //
+};
+
+struct RefList : public std::vector<List::const_iterator> {
+  RefList(const List& list) : _list{list} {
+    for (auto it = list.begin(); it != list.end(); ++it) {
+      emplace_back(it);
+    }
+  }
+  auto currentpos(auto number) const;
   auto nextpos(auto current_pos, auto increment) const;
+  auto shuffle();
+
+ private:
+  const List& _list;
 };
 
 inline auto& operator>>(std::istream& is, Number& number) {
+  static int id = 0;
   is >> number.value;
+  number.id = id++;
   return is;
 }
 
